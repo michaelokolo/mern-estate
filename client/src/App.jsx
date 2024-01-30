11af6c98c7750ee92a46909ae9b66e9bbbce787f
Michael Okolo <michaelokolo62@yahoo.com>
2024-01-30 07:14:42 -0500

    add redux tool kit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
-import SignIn from './pages/Signin';
+import SignIn from './pages/SignIn';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
