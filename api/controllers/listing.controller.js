601174fc253b9d245d1e74879a4954b9b62c9747
Michael Okolo <michaelokolo62@yahoo.com>
2024-02-08 23:42:44 -0500

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -28,3 +28,28 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found'));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only update your own listings!'));
+  }
+  if (!listing._id || String(listing._id) !== req.params.id) {
+    return next(errorHandler(404, 'Listing ID mismatch or not found'));
+  }
+
+  try {
+    const updateListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updateListing);
+  } catch (error) {
+    next(error);
+  }
+};
